76a34b22f93c81f35a34dd55e63247686a2224f1
zhaobing <zb@d2yun.com>
2023-04-17 15:59:49 +0800

    mqtt测试

--- main.go
+++ main.go
@@ -1,120 +1,71 @@
 package main
 
-// 与后端mqtt服务交互
-
 import (
 	"fmt"
-	"log"
-	"os"
-	"strconv"
+	"sync"
 	"time"
 
 	mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
-const EMQServerAddress = "你的IP"
+// 12847(mqtt), 12173(mqtts), 8083(ws), 8084(wss)
+const broker = "tcp://127.0.0.1:1883"
+const username = ""
+const password = ""
+const ClientID = "go_mqtt1_client"
 
-// 创建全局mqtt publish消息处理 handler
-var messagePubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
-	fmt.Println("Push Message:")
-	fmt.Printf("TOPIC: %s\n", msg.Topic())
-	fmt.Printf("MSG: %s\n", msg.Payload())
+// message的回调
+var onMessage mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
+	fmt.Printf("[%s] -> %s\n", msg.Topic(), msg.Payload())
 }
 
-// 创建全局mqtt sub消息处理 handler
-var messageSubHandler mqtt.MessageHandler = func(client mqtt.Client, msg mqtt.Message) {
-	fmt.Println("收到订阅消息:")
-	fmt.Printf("Sub Client Topic : %s \n", msg.Topic())
-	fmt.Printf("Sub Client msg : %s \n", msg.Payload())
-}
-
-// 连接的回掉函数
-var connectHandler mqtt.OnConnectHandler = func(client mqtt.Client) {
-	fmt.Println("新的连接!" + " Connected")
-}
-
-// 丢失连接的回掉函数
-var connectLostHandler mqtt.ConnectionLostHandler = func(client mqtt.Client, err error) {
-	fmt.Printf("Connect loss: %v\n", err)
-}
-
-func init() {
-	// 配置错误提示
-	mqtt.DEBUG = log.New(os.Stdout, "		[mqttDEBUG]", 0)
-	mqtt.ERROR = log.New(os.Stdout, " 	[mqttERROR]", 0)
-}
-
-/**
- * @Description: 发布订阅
- * @param clientID
- * @param addr
- * @param topic
- * @param payload
- */
-func Push(topic string, qos byte, retain bool, payload string) {
-	// opts ClientOptions 用于设置 broker，端口，客户端 id ，用户名密码等选项
-	opts := mqtt.NewClientOptions().AddBroker("tcp://" + EMQServerAddress + ":1883").SetClientID("test_push")
-	opts.SetKeepAlive(60 * time.Second)
-	// Message callback handler，在没有任何订阅时，发布端调用此函数
-	opts.SetDefaultPublishHandler(messagePubHandler)
-	opts.SetPingTimeout(1 * time.Second)
-	opts.OnConnect = connectHandler
-	opts.OnConnectionLost = connectLostHandler
-	client := mqtt.NewClient(opts)
-	if token := client.Connect(); token.Wait() && token.Error() != nil {
-		panic(token.Error())
-	}
-	//发布消息
-	// qos是服务质量: ==1: 一次, >=1: 至少一次, <=1:最多一次
-	// retained: 表示mqtt服务器要保留这次推送的信息，如果有新的订阅者出现，就会把这消息推送给它（持久化推送）
-	token := client.Publish(topic, qos, retain, payload)
-	token.Wait()
-	fmt.Println("Push Data : "+topic, "Data Size is "+strconv.Itoa(len(payload)))
-	fmt.Println("Disconnect with broker")
-	client.Disconnect(250)
-}
-
-/**
- * @Description: 订阅与取消订阅
- * @param clientID
- * @param addr
- * @param topic
- * @param isSub
- */
-func Subscription(topic string, qos byte, isSub bool, handleFun func([]byte)) {
-	opts := mqtt.NewClientOptions().AddBroker("tcp://" + EMQServerAddress + ":1883").SetClientID("sub_test")
-	opts.SetKeepAlive(60 * time.Second)
-	opts.SetPingTimeout(1 * time.Second)
-	opts.OnConnect = func(client mqtt.Client) {
-		fmt.Println("New Subscription! Connected" + " => " + topic)
-	}
-	opts.OnConnectionLost = connectLostHandler
-	client := mqtt.NewClient(opts)
-	if token := client.Connect(); token.Wait() && token.Error() != nil {
-		panic(token.Error())
-	}
-
-	if isSub {
-		// 订阅消息
-		if token := client.Subscribe(topic, qos, func(client mqtt.Client, msg mqtt.Message) {
-			fmt.Printf("Receive Subscribe Message :")
-			fmt.Printf("Sub Client Topic : %s, Data size is  %d \n", msg.Topic(), len(msg.Payload()))
-			if len(msg.Payload()) > 0 {
-				handleFun(msg.Payload())
-			}
-		}); token.Wait() && token.Error() != nil {
-			fmt.Println(token.Error())
-			os.Exit(1)
-		}
-	} else {
-		// 取消订阅
-		if token := client.Unsubscribe(topic); token.Wait() && token.Error() != nil {
-			fmt.Println(token.Error())
-			os.Exit(1)
-		}
-	}
-}
+var wg sync.WaitGroup
+var client mqtt.Client
 
 func main() {
-	fmt.Println("123")
+	//连接MQTT服务器
+	mqttConnect()
+	defer client.Disconnect(250) //注册销毁
+	wg.Add(1)
+	go mqttSubScribe("topic/test")
+	wg.Add(1)
+	go testPublish()
+	wg.Wait()
+}
+func mqttConnect() {
+	//配置
+	opts := mqtt.NewClientOptions()
+	opts.AddBroker(broker)
+	opts.SetClientID(ClientID)
+	// opts.SetUsername("")
+	// opts.SetPassword("")
+	opts.SetConnectTimeout(time.Duration(60) * time.Second)
+	//连接
+	client = mqtt.NewClient(opts)
+	//客户端连接判断
+	if token := client.Connect(); token.WaitTimeout(time.Duration(60)*time.Second) && token.Wait() && token.Error() != nil {
+		fmt.Println("订阅 MQTT 失败")
+		panic(token.Error())
+	}
+}
+
+func mqttSubScribe(topic string) {
+	defer wg.Done()
+	for {
+		// now := time.Now()
+		// fmt.Println(now.Format("2006-01-02 15:04:05.000 Mon Jan"))
+		token := client.Subscribe(topic, 1, onMessage)
+		token.Wait()
+	}
+}
+
+// 测试 3秒发送一次，然后自己接收
+func testPublish() {
+	defer wg.Done()
+	for {
+		now := time.Now()
+		//fmt.Println(now.Format("2006-01-02 15:04:05.000 Mon Jan"))
+		client.Publish("topic/test", 1, false, now.Format("2006-01-02 15:04:05.000 Mon Jan"))
+		time.Sleep(time.Duration(3) * time.Second)
+	}
 }
